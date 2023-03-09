0009e48ce97453312a3e99999789e04477c1ae33
matthewcroughan <matt@croughan.sh>
2023-03-09 00:08:29 +0000

    foo

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
+        "aarch64-linux"
       ];
       imports = [
         hercules-ci-effects.flakeModule
